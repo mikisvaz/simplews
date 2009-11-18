a18856974f54fe721205a90c0ee0df1fc81aac43
Miguel Vazquez <miguel.vazquez@fdi.ucm.es>
2009-11-18 16:16:52 +0100

    Added parens to sample client

--- samples/synchronous/simple/birthday_client.rb
+++ samples/synchronous/simple/birthday_client.rb
@@ -4,7 +4,7 @@ client = SOAP::WSDLDriverFactory.new( "BirthdayWS.wsdl").create_rpc_driver
 
 puts client.message
 
-puts client.greet 'Mike', 32
+puts client.greet('Mike', 32)
 
 %w(Mike Lisa).each do |name|
   if client.greeted name
