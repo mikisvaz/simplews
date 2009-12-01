499beda7d1ea3df6ae461a7d487586d8a3d28c9a
Miguel Vazquez <miguel.vazquez@fdi.ucm.es>
2009-12-01 23:23:07 +0100

    Improved rake support

--- lib/simplews/rake.rb
+++ lib/simplews/rake.rb
@@ -5,17 +5,37 @@ require 'rake'
 
 class SimpleWS::Jobs::Scheduler::Job
 
+  def add_message(reg_exp, step, message)
+    @step_messages ||= {}
+    @step_messages[Regexp.new(reg_exp)] = "#{ step }: #{ message }"
+  end
+
   def rake(rakefile = "Rakefile")
     $_current_job = self
+    $_step_messages = @step_messages || {}
     Rake::Task.class_eval <<-'EOC'
-      alias_method :old_invoke, :invoke
-      def invoke
-        if comment.match(/^(\w+): (.*)/)
+      alias_method :old_execute, :execute
+      def execute(*args)
+        action = name
+        message = $_step_messages.collect{|rexp, msg| 
+          if name.match(rexp)
+            msg
+          else
+            nil
+          end
+        }.compact.first
+
+        message ||= comment 
+
+        message ||= "Invoking #{name}"
+
+        if message.match(/^(\w+): (.*)/)
           $_current_job.step($1, $2)
         else
-          $_current_job.step(name, comment)
+          $_current_job.step(name, message)
         end
-        old_invoke
+
+        old_execute(*args)
       end
     EOC
 
