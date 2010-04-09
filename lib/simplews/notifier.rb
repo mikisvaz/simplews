6edc789dc8c5621e808be97027b4f7cc4590dc71
Miguel Vazquez <miguel.vazquez@fdi.ucm.es>
2010-04-09 18:11:35 +0200

    Email notifier

---
+++ lib/simplews/notifier.rb
@@ -0,0 +1,123 @@
+require 'rmail'
+require 'net/smtp'
+require 'soap/wsdlDriver'
+require 'simplews/jobs'
+
+class SimpleWS::Jobs::Notifier
+
+  def driver
+    case
+    when String === @ws
+      if File.exists? @ws
+        return SOAP::WSDLDriverFactory.new(@ws).create_rpc_driver
+      end
+    when Array === @ws
+      return SimpleWS.get_driver(@ws[0], @ws[1])
+    when SOAP::WSDLDriverFactory === @ws
+      return @ws
+    end
+
+    raise "Do not know how to connect to driver"
+  end
+
+  def initialize(name, host, ws, options = {})
+    @host = host
+    @name   = name
+    @ws     = ws
+    @smtp_host  = options[:smtp_host] || 'localhost'
+    @smtp_port  = options[:smtp_port] || 25
+    @sleep_time = options[:sleep_time] || 2
+    @jobs = {}
+  end
+
+  def add_job(job_id, email)
+    @jobs[job_id] = email
+  end
+
+  def process
+    @jobs.each do |job_id, email|
+      if driver.done job_id
+        if driver.error job_id
+          error(job_id, email, driver.messages(job_id).last)
+        else
+          success(job_id, email)
+        end
+        @jobs.delete(job_id)
+      end
+    end
+  end
+
+  def send_mail(to, subject, body)
+    puts "Sending mail to #{ to }: #{ subject }"
+    message = RMail::Message.new
+
+    from = "noreply@" + @host.sub(/:.*/,'')
+
+    message.header['To'] = to
+    message.header['From'] = from
+    message.header['Subject'] = subject
+    
+    main = RMail::Message.new    
+    main.body = body
+
+    message.add_part(main)
+
+    Net::SMTP.start(@smtp_host.chomp, @smtp_port.to_i) do |smtp|
+      smtp.send_message message.to_s, from, to
+    end
+  end
+
+
+  def error(job_id, email, msg)
+    body =<<-EOF
+Dear #{ @name } user:
+
+You job with id '#{ job_id }' has finished with error message:
+
+#{ msg }
+
+URL: http://#{@host.chomp}/#{ job_id }
+
+Note: Do not reply to this message, it is automatically generated.
+    EOF
+    send_mail(email, "#{@name} [ERROR]: #{ job_id }", body)
+  end
+
+  def success(job_id, email)
+    body =<<-EOF
+Dear #{ @name } user:
+
+You job with id '#{ job_id }' has finished successfully:
+
+URL: http://#{@host.chomp}/#{ job_id }
+
+Note: Do not reply to this message, it is automatically generated.
+    EOF
+    send_mail(email, "#{@name} [SUCCESS]: #{ job_id }", body)
+  end
+
+  def pending?
+    ! @jobs.empty?
+  end
+
+  def start
+    puts "Starting Email notifier."
+    puts "Name: #{ @name }"
+    puts "Host: #{ @host }"
+    puts "SMTP: #{ @smtp_host }"
+    @thread = Thread.new do
+      while true
+        begin
+          process
+          sleep @sleep_time
+        rescue
+          puts $!.message
+        end
+      end
+    end
+  end
+
+  def stop
+    @thread.kill
+  end
+end
---
+++ test/simplews/test_notifier.rb
@@ -0,0 +1,64 @@
+require File.dirname(File.dirname(__FILE__)) + '/test_helper.rb' 
+
+require 'simplews/notifier'
+require 'soap/wsdlDriver'
+
+MAIL="mikisvaz@gmail.com"
+#MAIL="change.to@your.mail.com"
+
+WSDL_FILE='TestMonitor_Simplews.wsdl'
+
+class TestMonitor < Test::Unit::TestCase
+  @@server = SimpleWS::Jobs.new do
+    task :test, %w(fail), {:fail => :boolean},[] do |do_fail|
+      step(:one, "One")
+      sleep 2
+      step(:two, "Two")
+      if do_fail
+        raise "Failed"
+      end
+    end
+  end
+
+  @@server.wsdl WSDL_FILE
+
+
+  @@monitor = SimpleWS::Jobs::Notifier.new("Test", "localhost:1984", WSDL_FILE, :smtp_host => 'ucsmtp.ucm.es')
+
+  @@driver = SOAP::WSDLDriverFactory.new(WSDL_FILE).create_rpc_driver
+
+  @@monitor.start
+  Thread.new do
+    @@server.start
+  end
+
+  #FileUtils.rm WSDL_FILE
+  
+  def test_error
+    @@monitor.error("job-1", MAIL, 'Test Error')
+  end
+
+  def test_success
+    @@monitor.success("job-1", MAIL)
+  end
+
+  def test_cicle_success
+   job = @@driver.test(false, '')
+   @@monitor.add_job(job, MAIL)
+
+   while @@monitor.pending?
+     sleep 10
+   end
+  end
+
+  def test_cicle_error
+   job = @@driver.test(true, '')
+   @@monitor.add_job(job, MAIL)
+
+   while @@monitor.pending?
+     sleep 10
+   end
+  end
+
+
+end
